b58503f97e005f133814e437291daf949183dcdb
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-03-11 11:57:22 +0000

    pasta js

---
+++ curso_js/class.js
@@ -0,0 +1,24 @@
+class Pessoa{
+
+    constructor(nome, idade){
+        this._nome = nome;
+        this._idade = idade;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    set nome(nome){
+        this._nome = nome
+    }
+
+    falar(msg){
+        console.log(`${this._nome} diz: ${msg}`);
+    }
+
+}
+
+const p1 = new Pessoa('Pedro', 19);
+
+p1.falar("Eae carai");
