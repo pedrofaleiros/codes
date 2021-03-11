1232336bf6ad0706d7442a34c8d4d9aefed298d6
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-03-11 16:14:09 -0300

    web

--- curso_js/ex01/ex01.js
+++ curso_js/ex01/ex01.js
@@ -1,12 +1,25 @@
 var num = 1
 
-
 function clicar(){
+    carregar()
     num += 1
     let aux = document.getElementById('clique')
     if(num % 2 == 0){
         aux.innerText = 'Sim'
     }else{
         aux.innerText = 'Nao'
+    }
+}
+
+function carregar(){
+    let txt = document.getElementById('hora')
+    let date = new Date()
+    let hora = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    if(min < 10){
+        txt.innerText = `${hora}:0${min}:${sec}`
+    }else{
+        txt.innerText = `${hora}:${min}:${sec}`
     }
 }
