13ad1432b865e76cb857872f2c96dfb03f43cd2e
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-03-28 15:32:44 +0000

    commit

--- testes/calculadora.h
+++ testes/calculadora.h
@@ -6,6 +6,26 @@
 #define or ||
 typedef int bool;
 
+int potencia(int num, int pot)
+{
+    int n = 1;
+    for(int i = 0; i < pot; i++){
+        n *= num;
+    }
+    return n;
+}
+
+int fatorial(int num)
+{
+    if(num > 0){
+        if(num == 1)
+            return 1;
+        else
+            return num * fatorial(num -1);
+    }else   
+        return 0;
+}
+
 typedef struct coordenada{
     int x;
     int y;
--- testes/main.c
+++ testes/main.c
@@ -1,38 +1,9 @@
 #include <stdio.h>
-#include "calculadora.c"
+#include "calculadora.h"
 
 int main()
 {
-    int n1, n2;
-    printf("\nsomar numeros: ");
-    scanf("%d %d", &n1, &n2);
-
-    int soma = somar(n1, n2);
-    int primo = verifica_primo(soma);
-
-    printf("\nSoma: %d\n", soma);
-    printf("Primo? ");
-    if(primo == True)
-        printf("sim\n");
-    else
-        printf("nao\n");
-
-    coordenada coord;
-
-    coord.x = n1;
-    coord.y = n2;
-
-    printf("\nCoordenadas: (%d, %d)\n", coord.x, coord.y);
-
-    if(n1 and n2)
-        printf("\nsim\n");
-    else
-        printf("\nnao\n");
-
-    if(n1 or n2)
-        printf("\nsim\n");
-    else
-        printf("\nnao\n");
+    printf(" %d\n", potencia(4, 3));
 
     return 0;
 }
