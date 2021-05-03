fcc53371a3eb4dbbf4e3c1704b8507ce62f47a04
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-05-03 18:44:34 +0000

    commit

--- estrutura_de_dados/algoritmos/insertion.c
+++ estrutura_de_dados/algoritmos/insertion.c
@@ -11,53 +11,75 @@ Monte um gráfico da curva do aumento de tempo de execução conforme se aumenta
 #include <time.h>
 #include "omp.h"
 
-long long int * aloca_vetor(long long int n);
+long long int * aloca_vetor(long long int num);
+void insertionsort(long long int * vet, long long int tam);
 
 int main()
 {
-    long long int *vet;
-    long long int tam;
-
     srand(time(NULL));
-    double t1,t2;
-    FILE * arq;
 
-    arq = fopen("tempos.csv","a+");
-    if(arq==NULL)
+    FILE * fl;
+
+    fl = fopen("tempos.csv", "a");
+    if(fl == NULL)
         return 0;
 
-    for(tam = 10000; tam <= 200000; tam = tam + 10000)
-    {
-        printf("\n Tamanho do vetor: %lld",tam);
-        vet = cria_vetor(tam);
-        t1 = omp_get_wtime();
-        bubblesort(vet, tam);
-        t2 = omp_get_wtime();
-        printf("\n Tempo para ordenar: %f",t2-t1);
-        fprintf(arq,"%lld;%f\n",tam,t2-t1);
-        free(vet);
-    }
-    fclose(arq);
+    long long int * vetor;
+    long long int n, i;
+    double t1, t2;
+
+    printf("\n Numero N: ");
+    scanf("%lld", &n);
+
+    vetor = aloca_vetor(n);
+    
+    t1 = omp_get_wtime();
+    insertionsort(vetor, n);
+    t2 = omp_get_wtime();
+
+    printf("\n tempo decorrido: %f", t2-t1);
+    fprintf(fl,"%lld;%f\n",n,t2-t1);
+    fclose(fl);
 
     printf("\n");
     return 0;
 }
 
-long long int * aloca_vetor(long long int n)
+void insertionsort(long long int * vet, long long int tam)
 {
-    long long int *vet, i;
-    vet = (long long int *)calloc(n, sizeof(long long int));
+    long long int i, j, aux;
 
-    for(i = 0; i < n; i++){
-        vet[i] = i + 1;
+    for(i = 1; i< tam; i++){
+        aux = vet[i];
+        int sair = 0;
+        for(j = i - 1; j>= 0 && !sair; j--){
+            if(vet[j] > aux){
+                vet[j+1] = vet[j];
+                vet[j] = aux;
+            }else{
+                vet[j+1] = aux;
+                sair = 1;
+            }
+        }
+    }
+}
+
+long long int * aloca_vetor(long long int num)
+{
+    long long int * vet, i, aux, aleatorio;
+
+    vet = calloc(num, sizeof(long long int));
+
+    for(i = 0; i < num; i++){
+        vet[i] = i + 1; 
     }
 
-    for(i = 0; i < n; i++){
-        vet[i] = rand() % n;
+    for(i = 0; i < num; i++){
+        aux = vet[i];
+        aleatorio = rand() % num;
+        vet[i] = vet[aleatorio];
+        vet[aleatorio] = aux;
     }
 
     return vet;
 }
-
-
-
