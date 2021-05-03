56dfe315ec7ddc4c4345af2248b97412bd74d591
pedrofaleiros <62016599+pedrofaleiros@users.noreply.github.com>
2021-05-03 11:11:49 -0300

    Create insertion.c

---
+++ insertion.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "omp.h"
+
+long long int * aloca_vetor(long long int num);
+void insertionsort(long long int * vet, long long int tam);
+
+int main()
+{
+    srand(time(NULL));
+
+    long long int * vetor;
+    long long int n, i;
+    double t1, t2;
+
+    printf("\n Numero N: ");
+    scanf("%lld", &n);
+
+    vetor = aloca_vetor(n);
+    
+    t1 = omp_get_wtime();
+    insertionsort(vetor, n);
+    t2 = omp_get_wtime();
+
+    printf("\n tempo decorrido: %f", t2-t1);
+
+    return 0;
+}
+
+void insertionsort(long long int * vet, long long int tam)
+{
+    long long int i, j, aux;
+
+    for(i = 1; i< tam; i++){
+        aux = vet[i];
+        int sair = 0;
+        for(j = i - 1; j>= 0 && !sair; j--){
+            if(vet[j] > aux){
+                vet[j+1] = vet[j];
+                vet[j] = aux;
+            }else{
+                vet[j+1] = aux;
+                sair = 1;
+            }
+        }
+    }
+}
+
+long long int * aloca_vetor(long long int num)
+{
+    long long int * vet, i, aux, aleatorio;
+
+    vet = calloc(num, sizeof(long long int));
+
+    for(i = 0; i < num; i++){
+        vet[i] = i + 1; 
+    }
+
+    for(i = 0; i < num; i++){
+        aux = vet[i];
+        aleatorio = rand() % num;
+        vet[i] = vet[aleatorio];
+        vet[aleatorio] = aux;
+    }
+
+    return vet;
+}
