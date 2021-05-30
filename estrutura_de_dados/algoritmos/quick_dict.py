d0acb7a656da29614feb23809d95e77c288838d8
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-05-30 11:19:43 -0300

    bubble

--- estrutura_de_dados/algoritmos/quick_dict.py
+++ estrutura_de_dados/algoritmos/quick_dict.py
@@ -48,6 +48,35 @@ def quicksort(pessoas, inicio, final):
     quicksort(pessoas, inicio, i-1)
     quicksort(pessoas, i+1, final)
 
+def bubblesort(pessoas, tamanho):
+    trocou = True
+
+    i = 0
+    while i < tamanho-1 and trocou:
+
+        trocou = False
+        j = 0
+        while j < tamanho-1-i:
+            if pessoas[j]['preco'] > pessoas[j+1]['preco']:
+                trocou = True
+
+                aux = pessoas[j+1]['nome']
+                pessoas[j+1]['nome'] = pessoas[j]['nome']
+                pessoas[j]['nome'] = aux
+
+                aux = pessoas[j+1]['preco']
+                pessoas[j+1]['preco'] = pessoas[j]['preco']
+                pessoas[j]['preco'] = aux
+
+                aux = pessoas[j+1]['materia']
+                pessoas[j+1]['materia'] = pessoas[j]['materia']
+                pessoas[j]['materia'] = aux
+
+            j += 1
+
+        i += 1
+
+
 pessoas = []
 
 for i in range(5):
@@ -59,7 +88,7 @@ for i in range(5):
 for pessoa in pessoas:
     print(pessoa)
 
-quicksort(pessoas, 0, len(pessoas)-1)
+bubblesort(pessoas, len(pessoas))
 
 print("ordenado:")
 for pessoa in pessoas:
