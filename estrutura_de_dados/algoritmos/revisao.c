64dedc8bcf907e5c37aee64ff4df3f84515e3e89
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-06-12 15:41:13 +0000

    revisao

--- estrutura_de_dados/algoritmos/revisao.c
+++ estrutura_de_dados/algoritmos/revisao.c
@@ -6,6 +6,9 @@ typedef long long int llint;
 
 llint * aloca_vetor(llint num);
 void mostra_vetor(llint * vet, llint tamanho);
+int ordenado(llint * vet, llint tamanho);
+void bubblesort(llint * vet, llint tamanho);
+void insertionsort(llint * vet, llint tamanho);
 
 
 int main()
@@ -14,21 +17,61 @@ int main()
 
     llint * vet, num;
 
-    num = 5;
+    num = 10;
 
     vet = aloca_vetor(num);
 
+    insertionsort(vet, num);
     mostra_vetor(vet, num);
 
-
+    ordenado(vet, num) == 1 ? printf("\nsim") : printf("\nnao");
 
     printf("\n");
     return 0;
 }
 
+void insertionsort(llint * vet, llint tamanho)
+{
+
+
+}
+
+void bubblesort(llint * vet, llint tamanho)
+{
+    llint i, j, aux, trocou;
+
+    trocou = 1;
+
+    for(i = 0; i < tamanho-1 && trocou; i++){
+
+        trocou = 0;
+
+        for(j = 0; j < tamanho - i - 1; j++){
+            if(vet[j] > vet[j+1]){
+                aux = vet[j];
+                vet[j] = vet[j+1];
+                vet[j+1] = aux;
+
+                trocou = 1;
+            }
+        }
+    }
+}
+
+int ordenado(llint * vet, llint tamanho)
+{
+    llint i;
+
+    for(i = 1; i < tamanho; i++)
+        if(vet[i] < vet[i-1])
+            return 0;
+        
+    return 1;
+}
+
 llint * aloca_vetor(llint num)
 {
-    llint i, aux, * vet;
+    llint i, j, aux, * vet;
 
     vet = calloc(num, sizeof(llint));
 
@@ -36,14 +79,11 @@ llint * aloca_vetor(llint num)
         vet[i] = i+1;
 
     for(i = 0; i < num; i++){
-        
-        aux = rand()%num;
-        while(aux == i)
-            aux = rand()%num;
+        j = rand()%num;
 
-        vet[i] = vet[i] + vet[aux];
-        vet[aux] = vet[i] - vet[aux];
-        vet[i] =  vet[i] - vet[aux];
+        aux = vet[i];
+        vet[i] = vet[j];
+        vet[j] = aux;
     }
     
     return vet;
@@ -53,6 +93,7 @@ void mostra_vetor(llint * vet, llint tamanho)
 {
     llint i;
 
+    printf("\n");
     for(i = 0; i < tamanho; i++)
-        printf("\n %lld", vet[i]);
+        printf(" %lld", vet[i]);
 }
