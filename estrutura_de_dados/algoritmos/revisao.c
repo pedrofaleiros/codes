61eab26a8fd5801beb3de425dd8935741cf978d3
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-06-13 13:37:31 +0000

    revisao

--- estrutura_de_dados/algoritmos/revisao.c
+++ estrutura_de_dados/algoritmos/revisao.c
@@ -7,8 +7,12 @@ typedef long long int llint;
 llint * aloca_vetor(llint num);
 void mostra_vetor(llint * vet, llint tamanho);
 int ordenado(llint * vet, llint tamanho);
+
 void bubblesort(llint * vet, llint tamanho);
 void insertionsort(llint * vet, llint tamanho);
+void selectionsort(llint * vet, llint tamanho);
+void mergesort(llint * vet, llint inicio, llint final);
+void merge(llint * vet, llint inicio, llint meio, llint final);
 
 
 int main()
@@ -17,11 +21,11 @@ int main()
 
     llint * vet, num;
 
-    num = 10;
+    num = 15;
 
     vet = aloca_vetor(num);
 
-    insertionsort(vet, num);
+    mergesort(vet, 0, num-1);
     mostra_vetor(vet, num);
 
     ordenado(vet, num) == 1 ? printf("\nsim") : printf("\nnao");
@@ -30,8 +34,85 @@ int main()
     return 0;
 }
 
+void mergesort(llint * vet, llint inicio, llint final)
+{
+    if(inicio < final){
+        llint meio;
+        meio = (inicio+final)/2;
+        mergesort(vet, inicio, meio);
+        mergesort(vet, meio+1, final);
+        merge(vet, inicio, meio, final);
+    }
+}
+
+void merge(llint * vet, llint inicio, llint meio, llint final)
+{
+    llint *novo, i, j, k;
+
+    novo = calloc(final-inicio+1, sizeof(llint));
+
+    i = inicio;
+    j = meio+1;
+    k = 0;
+
+    while(i <= meio && j <= final){
+        if(vet[i] <= vet[j]){
+            novo[k] = vet[i];
+            i++;
+        }else{
+            novo[k] = vet[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i <= meio){
+        novo[k] = vet[i];
+        k++;
+        i++;
+    }
+
+    while(j <= final){
+        novo[k] = vet[j];
+        k++;
+        j++;
+    }
+
+    for(i = 0; i < (final-inicio+1); i++){
+        vet[inicio+i] = novo[i];
+    }
+
+    free(novo);
+}
+
+void selectionsort(llint * vet, llint tamanho)
+{
+    llint i, j, aux, ind_menor;
+
+    for(i = 0; i < tamanho-1; i++){
+        ind_menor = i;
+        for(j = i; j < tamanho; j++){
+            if(vet[j] < vet[ind_menor]){
+                ind_menor = j;
+            }
+        }
+        aux = vet[i];
+        vet[i] = vet[ind_menor];
+        vet[ind_menor] = aux;
+    }
+}
+
 void insertionsort(llint * vet, llint tamanho)
 {
+    llint i, j, aux;
+
+    for(i = 1; i < tamanho; i++){
+        aux = vet[i];
+        for(j = i-1; j >= 0 && vet[j] > aux; j--){
+            vet[j+1] = vet[j];
+        }
+        vet[j+1] = aux;
+    }
 
 
 }
