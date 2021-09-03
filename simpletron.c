ed59b91081c52179fc99b3bc38573804bc872e3c
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-09-03 13:55:34 +0000

    simpletron

--- simpletron.c
+++ simpletron.c
@@ -1,50 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+#define HALT 43
+
 int acc = 0;
 int ip = 0;
 
 int mem[100];
 
-void read(int pos){ // 10
-    int num;
-
-    scanf("%d", &num);
-
-    mem[pos] = num;
-}
-
-void write(int pos){  // 11
-    printf("%d", mem[pos]);
-}
-
-void load(int pos){  // 20
-    acc = mem[pos];
-}
-
-void store(int pos){  // 21
-    mem[pos] = acc;
-}
-
-void add(int pos){  // 30
-    acc = acc + mem[pos];
-}
-
-void subtract(int pos){  // 31
-    acc = acc - mem[pos];
-}
-
-void divide(int pos){  // 32
-    acc = mem[pos] / acc;
-}
-
-void multiply(int pos){  // 33
-    acc = acc * mem[pos];
-}
-
-void halt(){ // 43
-    acc = -9999;
-}
+void read(int pos);
+void write(int pos);
+void load(int pos);
+void store(int pos);
+void add(int pos);
+void subtract(int pos);
+void divide(int pos);
+void multiply(int pos);
+void halt();
 
 void mostra();
 
@@ -69,47 +49,45 @@ int main(){
     int pos;
     ip = 0;
 
-    while(acc != -9999){
+    while(ip >= 0){
 
         pos = mem[ip] % 100;
 
         switch(mem[ip]/100){
-            case 10:
-                printf("read: ");
+            case READ:
+                printf("read [%d] = ", pos);
                 read(pos);
                 break;
-            case 11:
-                printf("write: ");
+            case WRITE:
+                printf("write [%d] = ", pos);
                 write(pos);
                 break;
-            case 20:
+            case LOAD:
                 load(pos);
                 break;
-            case 21:
+            case STORE:
                 store(pos);
                 break;
-            case 30:
+            case ADD:
                 add(pos);
                 break;
-            case 31:
+            case SUBTRACT:
                 subtract(pos);
                 break;
-            case 32:
+            case DIVIDE:
                 divide(pos);
                 break;
-            case 33:
+            case MULTIPLY:
                 multiply(pos);
                 break;
-            case 43:
+            case HALT:
                 halt();
                 break;
             default:
                 halt();
-                printf("ERRO");
+                printf("ERRO\n");
                 break;
         }
-
-        ip++;
 
     }
 
@@ -119,21 +97,92 @@ int main(){
     return 0;
 }
 
+void read(int pos)
+{
+    int num;
+
+    scanf("%d", &num);
+
+    mem[pos] = num;
+
+    ip++;
+}
+
+
+void write(int pos)
+{ 
+    printf("%d\n", mem[pos]);
+
+    ip++;
+}
+
+void load(int pos)
+{ 
+    acc = mem[pos];
+
+    ip++;
+}
+
+void store(int pos)
+{ 
+    mem[pos] = acc;
+
+    ip++;
+}
+
+void add(int pos)
+{ 
+    acc = acc + mem[pos];
+
+    ip++;
+}
+
+void subtract(int pos)
+{ 
+    acc = acc - mem[pos];
+
+    ip++;
+}
+
+void divide(int pos)
+{ 
+    acc = mem[pos] / acc;
+
+    ip++;
+}
+
+void multiply(int pos)
+{ 
+    acc = acc * mem[pos];
+
+    ip++;
+}
+
+void halt()
+{
+    ip = -1;
+}
+
 void mostra()
 {
+    printf("--------------------------------------------------------------------------------------------\n");
+    printf("acc = %d\n", acc);
+    printf("ip  = %d\n", ip);
+
     for(int i = 0; i < 100; i++){
 
-        if(i % 10 == 0){
-            printf("\n");
-        }else{
-            printf("\t");
-        }
-
         if(i < 10){
-            printf("[0%d] = %d", i, mem[i]);
+            if(i == 0){
+                printf("[0%d] = %d", i, mem[i]);
+            }else{
+                printf("\t[0%d] = %d", i, mem[i]);
+            }
         }else{
-            printf("[%d] = %d", i, mem[i]);
+            if(i % 10 == 0){
+                printf("\n[%d] = %d", i, mem[i]);
+            }else{
+                printf("\t[%d] = %d", i, mem[i]);
+            }
         }
-
     }
 }
